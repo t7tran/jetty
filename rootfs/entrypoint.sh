9faee63ed8ed870026b7f5c2c567c556c233d2cb
Tien Tran <tiendung.tran@gmail.com>
2020-08-22 13:06:04 +1000

    Fix issue with empty keystore file

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 if [[ -f "$CA_CERTIFICATE" ]]; then
-    cacerts=/usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts
+	cacerts=/usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts
 	keytool -import -keystore $cacerts -storepass changeit \
-	    -file $CA_CERTIFICATE -alias custom-root-ca -noprompt >/dev/null
+		-file $CA_CERTIFICATE -alias custom-root-ca -noprompt >/dev/null
 fi
 
 if [[ -f "$CERTIFICATE" && -f "$CERTIFICATE_KEY" && -n "$STORE_PASS" && -n "$KEY_PASS" ]]; then
@@ -15,11 +15,12 @@ if [[ -f "$CERTIFICATE" && -f "$CERTIFICATE_KEY" && -n "$STORE_PASS" && -n "$KEY
 		CMD="$CMD -CAfile $CA_CERTIFICATE -caname 'Root CA'"
 	fi
 	eval "$CMD -password pass:$STORE_PASS"
-	
+
+	keytool -storepasswd -new $STORE_PASS -keystore /keystore.jks -storepass storePassword &>/dev/null
 	keytool -importkeystore \
-	    -deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore /keystore.jks \
-        -srckeystore /keystore.p12 -srcstoretype PKCS12 -srcstorepass $STORE_PASS
-    echo > /keystore.p12
+		-deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore /keystore.jks \
+		-srckeystore /keystore.p12 -srcstoretype PKCS12 -srcstorepass $STORE_PASS
+	echo > /keystore.p12
 fi
 
 if [[ ! -z "$WAITFOR_HOST" && ! -z "$WAITFOR_PORT" ]]; then
