2dcee4bb297d9f7e2d93ef34aeaa9e2f67f74a78
Tien Tran <tien@transfamily.info>
2021-08-31 21:56:57 +1000

    Enhance entrypoint to import ca and certs into custom locations

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -7,27 +7,34 @@ set -e
 [[ -w /etc/time || -w /etc/time/timezone  ]] && echo $TZ > /etc/time/timezone
 
 if [[ -f "$CA_CERTIFICATE" ]]; then
-	cacerts=/usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts
-	[[ ! -w $cacerts ]] && echo $CA_CERTIFICATE cannot be added && exit 1
-	if keytool -list -keystore $cacerts -storepass changeit -alias custom-root-ca &>/dev/null; then
-		keytool -delete -keystore $cacerts -storepass changeit -alias custom-root-ca
+	cacerts=${CA_STORE:-/etc/ssl/certs/java/cacerts}
+	capass=${CA_PASS:-changeit}
+	if [[ ! -f $cacerts ]]; then
+		[[ ! -w `dirname $cacerts` ]] && echo `dirname $cacerts` is readonly && exit 1
+		keytool -genkeypair -alias boguscert -storepass $capass -keypass $capass -keystore $cacerts -dname "CN=Developer, OU=Department, O=Company, L=City, ST=State, C=CA"
+		keytool -delete -alias boguscert -storepass $capass -keystore $cacerts
 	fi
-	keytool -import -keystore $cacerts -storepass changeit -alias custom-root-ca -file $CA_CERTIFICATE -noprompt >/dev/null
+	[[ ! -w $cacerts ]] && echo $CA_CERTIFICATE cannot be added && exit 1
+	if keytool -list -keystore $cacerts -storepass $capass -alias custom-root-ca &>/dev/null; then
+		keytool -delete -keystore $cacerts -storepass $capass -alias custom-root-ca
+	fi
+	keytool -import -keystore $cacerts -storepass $capass -alias custom-root-ca -file $CA_CERTIFICATE -noprompt >/dev/null
 fi
 
 if [[ -f "$CERTIFICATE" && -f "$CERTIFICATE_KEY" && -n "$STORE_PASS" && -n "$KEY_PASS" ]]; then
-	[[ ! -w /opt/jetty/certs ]] && echo /opt/jetty/certs not writable && exit 1
-	CMD="openssl pkcs12 -export -in $CERTIFICATE -inkey $CERTIFICATE_KEY -out /opt/jetty/certs/keystore.p12"
+	keystore=${KEY_STORE:-/opt/jetty/certs/keystore.jks}
+	[[ ! -w `dirname $keystore` ]] && echo `dirname $keystore` not writable && exit 1
+	CMD="openssl pkcs12 -export -in $CERTIFICATE -inkey $CERTIFICATE_KEY -out ${keystore}.p12"
 	if [[ -f "$CA_CERTIFICATE" ]]; then
 		CMD="$CMD -CAfile $CA_CERTIFICATE -caname 'Root CA'"
 	fi
 	eval "$CMD -password pass:$STORE_PASS"
 
-	rm -rf /opt/jetty/certs/keystore.jks
+	rm -rf ${keystore}
 	keytool -importkeystore \
-		-deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore /opt/jetty/certs/keystore.jks \
-		-srckeystore /opt/jetty/certs/keystore.p12 -srcstoretype PKCS12 -srcstorepass $STORE_PASS
-	rm -rf /opt/jetty/certs/keystore.p12
+		-deststorepass $STORE_PASS -destkeypass $KEY_PASS -destkeystore ${keystore} \
+		-srckeystore ${keystore}.p12 -srcstoretype PKCS12 -srcstorepass $STORE_PASS
+	rm -rf ${keystore}.p12
 fi
 
 if [[ ! -z "$WAITFOR_HOST" && ! -z "$WAITFOR_PORT" ]]; then
